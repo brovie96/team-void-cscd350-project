56de4cb8eec987ef0c0c8897e7efd7ee279de027
brovie96 <seth.riedel@gmail.com>
2016-05-18 21:07:25 -0700

    Fix packages, part I

--- teamvoid/hero/I_HeroAttackMove.java
+++
@@ -1,5 +0,0 @@
-package teamvoid.hero;
-
-public interface I_HeroAttackMove {
-   public void getAttackMove(A_Hero a);
-}
--- teamvoid/potion/I_Potion.java
+++
@@ -1,6 +0,0 @@
-package teamvoid.potion;
-import  teamvoid.hero.*;
-
-public interface I_Potion {
-   public void potionBoost(A_Hero a);
-}
